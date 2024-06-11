3217e7e2b7aff32608b0599e5999779710ae1ad5
lucjovi2001 <171295225+lucjovi2001@users.noreply.github.com>
2024-06-11 09:16:56 -0700

    Add root and init output directory and file

--- converter.py
+++ converter.py
@@ -3,3 +3,12 @@ from tkinter import filedialog, messagebox, ttk
 from tkinterdnd2 import DND_FILES, TkinterDnD
 from PyPDF2 import PdfReader
 import os, time
+
+# main window
+root = TkinterDnD.Tk()
+root.title("PDF Converter")
+
+# widgets
+output_dir = tk.StringVar()
+output_file = tk.StringVar()
+selected_file = tk.StringVar()
